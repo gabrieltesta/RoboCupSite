c974a3de7ca6b97764131c5c6420b7c89b680704
Gabriel Testa <gabrielaugustotesta@gmail.com>
2019-08-23 22:26:03 -0300

    escrever titulo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
     var newVideo = document.getElementById('video_header');
     newVideo.addEventListener('ended', function() {
         this.currentTime = 0;
@@ -6,5 +6,53 @@ window.addEventListener('load', function(){
     }, false);
 
     newVideo.play();
+});
 
+function escreverTitulo(titulo, id) {
+    var _id = '#' + id;
+    var counter = parseInt($(_id).attr('data-counter'));
+    $(_id + ' span').remove();
+
+    if (counter < titulo.length) {
+        $(_id).html($(_id).html() + titulo.charAt(counter) + '<span class="text-pink">_</span>');
+        counter++;
+        $(_id).attr('data-counter', counter);
+        setTimeout(function() {
+            escreverTitulo(titulo, id);
+        }, 225);
+    } else {
+        $(_id).addClass('blink');
+    }
+
+}
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top - 74;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+var robocup_scroll, picanha_scroll = false;
+
+function animacoesTitulo() {
+    if (isScrolledIntoView($('#title_robocup')) && !$('#title_robocup').hasClass('blink') && !robocup_scroll) {
+        robocup_scroll = true;
+        escreverTitulo('O ROBOCUP', 'title_robocup');
+    }
+
+    if (isScrolledIntoView($('#title_picanha')) && !$('#title_picanha').hasClass('blink') && !picanha_scroll) {
+        picanha_scroll = true;
+        escreverTitulo('O PICANHA', 'title_picanha');
+    }
+}
+$(window).on('scroll', function() {
+    animacoesTitulo();
+});
+
+$(document).ready(function() {
+    animacoesTitulo()
 });
